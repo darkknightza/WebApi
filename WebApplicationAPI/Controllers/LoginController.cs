95beacaa421b1e4abc1c4c89d845d5a0f795f183
darkknightza <34331233+darkknightza@users.noreply.github.com>
2018-04-24 11:06:40 +0700

    ...

---
+++ WebApplicationAPI/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationAPI.Models;
+
+namespace WebApplicationAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Login")]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public IActionResult PostLogin([FromBody] Login login)
+        {
+            if (login.Username != null && login.Password != null)
+            {
+                if (login.Username.Equals("admin") && login.Password.Equals("1234"))
+                {
+                    return Ok("Success");
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ WebApplicationAPI/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicationAPI.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
